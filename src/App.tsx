c86123a3309e9473b9ee58208bf057df9c251a54
cani1see <mrt.left@gmail.com>
2021-04-16 23:54:06 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet,} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
